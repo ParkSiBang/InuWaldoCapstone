502e977a5dad3d7204b60756cc8408654df95f80
JoBob1218 <angel981218@gmail.com>
2023-04-26 02:40:15 +0900

    통합 진행중

--- frontend/Test/src/navigations/index.js
+++ frontend/Test/src/navigations/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import Auth from './Auth';
 
 const Navigation = () => {
     return <NavigationContainer>
+        <Auth />
     </NavigationContainer>
 }
 
