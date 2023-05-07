710e266692a2032797fd3955f7162b809931c1d7
JoBob1218 <angel981218@gmail.com>
2023-05-08 02:55:21 +0900

    card 및 버튼 수정

---
+++ frontend/Test1/src/screens/index.js
@@ -0,0 +1,7 @@
+import Signin from './Signin';
+import Signup from './Signup';
+import Profile from './Profile';
+import Map from './Map';
+import MapResult from './MapResult';
+
+export { Signin, Signup, Profile, Map, MapResult };
